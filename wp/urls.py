bb03cbc9ba77857cb542a53343d8129e03f38433
saadasghar9 <saadasgharofficial@gmail.com>
2025-02-26 21:59:09 +0500

    removed root line

--- wp/urls.py
+++ wp/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('receiver.urls')),
-    path('', include('receiver.urls')),  # This will catch the root URL
+    # path('', include('receiver.urls')),  # This will catch the root URL
 ]
 
